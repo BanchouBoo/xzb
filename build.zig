5d563bb3a7d4310900aef1cd133de0a2a4eb1bcb
Calla <32691832+BanchouBoo@users.noreply.github.com>
2023-03-07 19:34:11 -0500

    Fix build.zig

--- build.zig
+++ build.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{
-        .name = "xzb",
+    _ = b.addModule("xzb", .{
         .source_file = std.Build.FileSource.relative("xzb.zig"),
         .dependencies = &[_]std.Build.ModuleDependency{},
     });
