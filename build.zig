cb0959e9679ec84ff55d55fd3b4444b3ae9737f5
Calla <32691832+BanchouBoo@users.noreply.github.com>
2024-07-01 16:32:53 -0400

    Update for zig master

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("xzb", .{
-        .root_source_file = std.Build.LazyPath.relative("xzb.zig"),
+        .root_source_file = b.path("xzb.zig"),
     });
 }
